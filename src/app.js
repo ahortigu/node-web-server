fe34920e102c90f9ad4084bafb3909dd1e90161b
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-21 16:54:24 +0000

    Templates - partials & views

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 //console.log(__dirname)
 // console.log(path.join(__dirname,'../public'))
 const app = express()
@@ -11,16 +12,40 @@ const app = express()
 //second argument 9(reposnse)
 
 
+// Define paths for Express confi
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlers engine and views location 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to server
 app.use(express.static(publicDirectoryPath))
 
+app.get('',(req,res) => {
+    res.render('index',{
+        title:'Weather App',
+        name:'Ana Garcia'
+    })
+}) 
+ 
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title:'About',
+        name:'Ana Garcia'
+    })
+}) 
 
-app.get('', (req,res) => {
-  // res.send is sending back a response 
-    res.send ('<h1>Weather<h1>')
-})
-
+app.get('/help',(req,res) => {
+    res.render('help',{
+        title:'help',
+        name:'Ana Garcia',
+        helpText:'In this website you can find any help you may need'
+    })
+}) 
 
 app.get('/weather', (req, res) =>{
     res.send({
@@ -33,3 +58,4 @@ app.listen(3000, () =>{
     console.log('Server is up on port 3000')
 
 })
+
