4a0d2010d638638f722e33fa115980eca627879d
Ana HG <anaisabel.hortiguela@gmail.com>
2020-01-22 17:59:37 +0000

    Linking search Button  with location

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,30 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-     response.json().then((data) => {
-         console.log(data)
-     })
 
- })
+ const weatherForm = document.querySelector('form')
+ const search = document.querySelector('input')
+ const messageOne= document.querySelector('#message-1')
+ const messageTwo= document.querySelector('#message-2')
 
-// fetch('http://localhost:3000/weather?address=London').then((response) => {
-//     response.json().then((data) =>{
-//         if(data.error) {
-//             console.log(data.error)
-//         }else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
+weatherForm.addEventListener('submit',(e) =>{
+     //Preventing to refresh by default
+     e.preventDefault()
+
+     const location = search.value
+
+     messageOne.textContent = 'Loading...' 
+     messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                console.log(messageOne.textContent = data.error)
+            }else{
+            console.log(messageOne.textContent = data.location)
+            console.log(messageTwo.textContent = data.forecast)
+            }
+
+        })
+    })
+
+})
